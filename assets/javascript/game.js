33caaabc9b86548c8a2103ff322374dafda70996
SueJStevens <StevensStock@gmail.com>
2018-08-24 17:01:28 -0700

    Working ready to style

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -18,6 +18,7 @@ function changeDisplay(strElementID, displayValue) {
 //variables
 var initial_guessesLeft = 9;
 var initial_guesses = "";
+var initial_rightChoices = "";
 var winCounter = 0; //never gets reset back to zero
 var lossCounter = 0; //never gets reset back to zero
 var guessesLeft = initial_guessesLeft;
@@ -25,24 +26,30 @@ var computerPick = " ";
 var userChoices = initial_guesses;
 var userChoice = "";
 
-var animals = ['terrapin','bear','dire wolf','china cat','bird song', 'carrion crow'];
+var weather = ['snow and rain','box of rain','wind','bolt','weather report','still water', 'thunder'];
 var wordBlank = "";
 var rightChoices = "";
+
+var arrEmoji = ['\xa0' + '\u23A7' + 'RIP' + '\u23AB' + '\xa0', '\xa0' + '\u23AB' ,  '\u23A7' + '\xa0', '\u0029', '\u003B', '\u00D7', '\u005F', '\u00D7', '\u0028'];
+var emoji = "";
+var emoji1 = "";
 
 //call reset function
 
 function resetGame() {
 
     //reset both display and internal variables
+    changeDisplay("lastWord", computerPick);
     changeDisplay("guessesLeft", initial_guessesLeft);
     changeDisplay("wrongGuesses", initial_guesses);
 
     //reset guessesLeft and guesses
     guessesLeft = initial_guessesLeft;
     userChoices = initial_guesses; 
+    rightChoices = initial_rightChoices;
     
     //Computer Picks Random Letter and changes display
-    computerPick = animals[getRandomInt(animals.length)];
+    computerPick = weather[getRandomInt(weather.length)];
     changeDisplay("computerPick", computerPick);
     
     //length of word computer picked
@@ -78,6 +85,33 @@ function stringMask(str, str2='') {
 
 }
 
+function buildEmoji(int) {
+
+    //int === 9 means the user chose a correct letter so erase the emoji
+    if (int === 9) {
+        emoji = "";
+        emoji1 = "";
+        changeDisplay("emoji", emoji);
+        changeDisplay("emoji1", emoji1);
+    //int === 8 means the user chose an incorrect letter so the emoji still needs to be erased, but then rebuild starts
+    } else if (int === 8) {
+        emoji = "";
+        emoji1 = "";
+        changeDisplay("emoji", emoji);
+        changeDisplay("emoji1", emoji1);
+        emoji = emoji + arrEmoji[int];
+    } else if (int <9 && int > 2) {
+        emoji = emoji + arrEmoji[int];
+    } else if (int >0) {
+        emoji1 = emoji1 + arrEmoji[int];
+    } else {
+        emoji1 = arrEmoji[int];
+    }
+    changeDisplay("emoji", emoji);
+    changeDisplay("emoji1", emoji1);
+
+}
+    
 
 //Start the game
 resetGame();
@@ -109,6 +143,8 @@ document.onkeyup = function(event) {
                 //decrease guesses left
                 guessesLeft = guessesLeft - 1;
                 changeDisplay("guessesLeft", guessesLeft);
+                buildEmoji(guessesLeft);
+
 
                 //test to see if game over
                 if (guessesLeft === 0) {
@@ -116,6 +152,8 @@ document.onkeyup = function(event) {
                     //increment losses
                     lossCounter = lossCounter + 1;
                     changeDisplay("lossCounter", lossCounter);
+
+                    //Reset Game
                     resetGame()
                 }
                 
@@ -124,6 +162,16 @@ document.onkeyup = function(event) {
                 //changeDisplay("userChoice", userChoice);
 
             } else {
+
+                //fix emoji if this is the first play
+                if (guessesLeft === 9) {
+                    emoji = "";
+                    emoji1 = "";
+                    changeDisplay("emoji", emoji);
+                    changeDisplay("emoji1", emoji1);            
+                }
+
+
                 //display the user choices as right guesses in the random word display
                 rightChoices = rightChoices + userChoice;
                 //reformatting the mask to include the right letters entered
